5354f9f9740e96e85d9f2871af24189cba68b26b
Andrew Gnias <AGnias47@gmail.com>
2017-07-17 03:18:04 +0000

    Workaround added for historical data error; should look into other apis as this is really slow

--- acorns.py
+++ acorns.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
+#
+#   acorns.py - Finance app to show performance of Acorns allocations.
+#
+#   A. Gnias
+#   Created: 7/16/2017
+#
+#   Python 3.4.2
+#   Linux raspberrypi 4.1.7+
+#   Vim 7.4 - tabstop=3
 
 from yahoo_finance import Share
-import pandas
+import urllib.request
+from bs4 import BeautifulSoup
 
 def daily_statistics(ETF) :
 	print("Daily Statistics")
@@ -21,6 +31,23 @@ def weekly_statistics(ETF) :
 	start = datetime.timedelta(weeks=1)
 	end = datetime.date.today()
 	print(start, end)
+
+
+def get_historical_data(name, number_of_days):
+	#Source: https://github.com/lukaszbanasiak/yahoo-finance/issues/128
+	#Note: Incredibly time consuming - 30 seconds for 15 days
+	data = []
+	url = "https://finance.yahoo.com/quote/" + name + "/history/"
+	rows = BeautifulSoup(urllib.request.urlopen(url).read(),"html5lib").findAll('table')[1].tbody.findAll('tr')
+
+	for each_row in rows:
+		divs = each_row.findAll('td')
+		if divs[1].span.text  != 'Dividend': #Ignore this row in the table
+			#I'm only interested in 'Open' price; For other values, play with divs[1 - 5]
+			data.append({'Date': divs[0].span.text, 'Open': float(divs[1].span.text.replace(',',''))})
+
+	return data[:number_of_days]
+
 
 def main() :
 	SP500_ETF = Share('VOO')
@@ -44,4 +71,5 @@ def main() :
 		print("")
 		i += 1
 
-weekly_statistics("fake")
+t = get_historical_data('amzn', 15)
+print(t)
